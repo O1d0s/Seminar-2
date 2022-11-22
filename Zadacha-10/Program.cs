bdb8c3f2da33c46efcfaed2ab13658dd06e3f6bb
Aidos <ajdos0735@gmaul.com>
2022-11-22 19:32:25 +0600

    находит 2 цифру

---
+++ Zadacha-10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length > 3){
+    Console.WriteLine("У этого чиcла больше 3 цифр");
+}
+else{
+    if (number.Length < 3){
+        Console.WriteLine("У этого чиcла меньше 3 цифр");
+    }
+    else{
+        Console.WriteLine(number[1]);
+    }
+}
+
+
