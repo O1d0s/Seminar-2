0d54210e53c47ee1f6054868d8cb1d52b9dfeb23
Aidos <ajdos0735@gmaul.com>
2022-11-22 19:59:27 +0600

    Сделал задачу-13

---
+++ Zadacha-13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Console.Write("Введите число: ");
+int inumber = int.Parse(Console.ReadLine());
+string number = inumber.ToString();
+
+if (inumber > 100000){
+    Console.WriteLine("Это очень большое число");
+}
+else{
+    if (number.Length < 3){
+        Console.WriteLine("У этого чиcла нет третьей цифры");
+    }
+    else{
+        Console.WriteLine(number[2]);
+    }
+}
+
+
+
+
