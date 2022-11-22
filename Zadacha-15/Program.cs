2e84bff8d15edb181c37e0f2abefd20d8bdd0266
Aidos <ajdos0735@gmaul.com>
2022-11-22 20:53:15 +0600

    Сделал Задачу-15

---
+++ Zadacha-15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a >= 1 && a <= 7){
+    if( a >= 1 && a <= 5 ){
+        Console.WriteLine("Нет");
+    }
+    else{
+        Console.WriteLine("Да");
+    }
+}
+else{
+    Console.WriteLine("Ошибка");
+}
